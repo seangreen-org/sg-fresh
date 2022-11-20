6fda8df4d1c77128e98b54055cc867dfeba4f03a
Sean Green <seangreen@hollandandbarrett.com>
2022-11-20 01:20:23 +0000

    dont send request on deployed

--- routes/api/hue.js
+++ routes/api/hue.js
@@ -2,23 +2,27 @@ import * as queryString from 'https://deno.land/x/querystring@v1.0.2/mod.js';
 import { getRandomInt } from '../../util.js';
 
 export const handler = {
-  async GET (req) {
+  GET (req) {
+    // !! how to color match this value?
     const rotation = queryString.parse(req.url.split('?')[1]).rotation;
 
-    console.log('send', rotation / 10);
-
-    fetch(
-      'https://192.168.86.30/api/MQdR757RjJg7Q6xgFtFSEN0jhi-XTQKLbwKKsD4G/lights/1/state',
-      {
-        method: 'PUT',
-        body: JSON.stringify({
-          on: true,
-          sat: 254,
-          bri: 254,
-          hue: getRandomInt(0, 96920)
-        })
-      }
-    );
+    if (!Deno.env.get('external')) {
+      console.log('at home... send fetch');
+      fetch(
+        'https://192.168.86.30/api/MQdR757RjJg7Q6xgFtFSEN0jhi-XTQKLbwKKsD4G/lights/1/state',
+        {
+          method: 'PUT',
+          body: JSON.stringify({
+            on: true,
+            sat: 254,
+            bri: 254,
+            hue: getRandomInt(0, 96920)
+          })
+        }
+      );
+    } else {
+      console.log('intruder alert!');
+    }
 
     return new Response();
   },
