eaa2a41ac1f160048430eb4df0a1dddf13578101
Sean Green <seangreen@hollandandbarrett.com>
2022-11-20 01:27:06 +0000

    logger?

--- routes/api/hue.js
+++ routes/api/hue.js
@@ -1,4 +1,5 @@
 import * as queryString from 'https://deno.land/x/querystring@v1.0.2/mod.js';
+import * as log from "https://deno.land/std/log/mod.ts";
 import { getRandomInt } from '../../util.js';
 
 console.info('HI!')
@@ -9,7 +10,7 @@ export const handler = {
     const rotation = queryString.parse(req.url.split('?')[1]).rotation;
 
     if (!Deno.env.get('external')) {
-      console.debug('at home... send fetch');
+      log.debug('at home... send fetch');
       fetch(
         'https://192.168.86.30/api/MQdR757RjJg7Q6xgFtFSEN0jhi-XTQKLbwKKsD4G/lights/1/state',
         {
@@ -23,7 +24,7 @@ export const handler = {
         }
       );
     } else {
-      console.debug('intruder alert!');
+      log.debug('intruder alert!');
     }
 
     return new Response();
