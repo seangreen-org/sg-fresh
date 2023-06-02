42b5f5d1ef23dc27cb41f3c7ad7d2c2cdae377e2
Sean Green <seangreen@hollandandbarrett.com>
2023-06-02 16:31:08 +0100

    hide waveform earlier

--- islands/LaserWaveform.jsx
+++ islands/LaserWaveform.jsx
@@ -129,7 +129,7 @@ const LaserWaveform = ({ audioAnalyserRef }) => {
         width: '100%',
         height: '100%',
         opacity: isActive ? 1 : 0,
-        transition: `opacity .5s ease-in-out`,
+        transition: `opacity .25s ease-in-out`,
       }}
     />
   );
