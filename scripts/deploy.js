35a50dcc43acac77b8844cee8fcae41ba367e281
Sean Green <seangreen@hollandandbarrett.com>
2023-07-04 14:43:45 +0100

    enable deploy (with deprecated Deno.run)

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,5 @@
 async function runCommandBailOnError(command) {
+  // deno-lint-ignore no-deprecated-deno-api
   const result = await (Deno.run({ cmd: command.split(' ') })).status();
   if (!result.success) {
     throw new Error(command, 'failed. bailing!');
