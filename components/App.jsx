115edea6cecfdc69e730b9817c008262fd7efaa4
Sean Green <seangreen@hollandandbarrett.com>
2025-01-18 00:20:37 +0000

    leave it out

--- components/App.jsx
+++ components/App.jsx
@@ -1,13 +1,12 @@
 import Header from '../components/Header.jsx';
-import BackgroundImage from '../islands/Background.jsx';
-import Heart from '../islands/Heart.jsx';
 
 export default function App(props) {
   return (
     <>
       <Header />
-      <BackgroundImage />
-      <Heart {...props} />
+      <div class="backdrop">
+        <img id="sg1981" src="/images/heart.svg" alt="love" />
+      </div>
     </>
   );
 }
