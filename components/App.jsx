b1c462d78483567915075681781de105de9049e9
Sean Green <seangreen@hollandandbarrett.com>
2025-01-18 01:03:54 +0000

    get owwwwwttttt

--- components/App.jsx
+++ components/App.jsx
@@ -1,12 +1,10 @@
 import Header from '../components/Header.jsx';
 
-export default function App(props) {
+export default function App(props = {}) {
   return (
     <>
       <Header />
-      <div class="backdrop">
-        <img id="sg1981" src="/images/heart.svg" alt="love" />
-      </div>
+      <img id="sg1981" src="/images/heart.svg" alt="love" />
     </>
   );
 }
