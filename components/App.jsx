af8b11f4e4b7efb698e41b30d20f8b24ee81d9fc
Sean Green <seangreen@hollandandbarrett.com>
2025-01-18 16:59:21 +0000

    safari visibility fix

--- components/App.jsx
+++ components/App.jsx
@@ -7,6 +7,7 @@ export default function App() {
       <svg
         id="sg1981"
         xmlns="http://www.w3.org/2000/svg"
+        width="100%" height="100%"
         viewBox="50 20 400 360"
       >
   <defs>
