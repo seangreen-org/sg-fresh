ab3b9131e0d658f43357c1d45d566112eb56257e
Sean Green <seangreen@hollandandbarrett.com>
2025-03-24 11:52:17 +0000

    add umami tracking

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -9,6 +9,7 @@ export default function App({ Component }: PageProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="/styles.css" />
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💚</text></svg>" />
+        <script defer src="https://cloud.umami.is/script.js" data-website-id="192417e2-392b-4ac0-bbbe-f1d0c25fb369"></script>
       </head>
       <body>
         <Component />
