5241b6368fb7bc114b542b3abc047307799b851d
Sean Green <seangreen@hollandandbarrett.com>
2025-03-24 16:22:18 +0000

    mucking about

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,8 +5,8 @@ export default function App({ Component }: PageProps) {
     <html lang="zxx">
       <head>
         <title>sg-1981</title>
-        <meta name="description" content="Sean Green"/>
-        <meta name="theme-color" content="#000000"/>
+        <meta name="description" content="Sean Green" />
+        <meta name="theme-color" content="#000000" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="/styles.css" />
         <link rel="icon" type="image/png" href="/favicons/favicon-96x96.png" sizes="96x96" />
@@ -14,7 +14,7 @@ export default function App({ Component }: PageProps) {
         <link rel="shortcut icon" href="/favicons/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
-        <script defer src="https://cloud.umami.is/script.js" data-website-id="192417e2-392b-4ac0-bbbe-f1d0c25fb369"></script>
+        <script defer src="https://cloud.umami.is/script.js" data-website-id="192417e2-392b-4ac0-bbbe-f1d0c25fb369" />
       </head>
       <body>
         <Component />
