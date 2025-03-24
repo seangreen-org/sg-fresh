61100019b7992521e9777c78a1572cb1f632750d
Sean Green <seangreen@hollandandbarrett.com>
2025-03-24 12:22:42 +0000

    track server colour change event

--- routes/api/hue.ts
+++ routes/api/hue.ts
@@ -1,9 +1,39 @@
 const isInternal = () =>
   JSON.stringify(Deno.env.get('SG_NETWORK')) === JSON.stringify('internal');
 
+const trackColorChangeEvent = async (request: Request, color: string) => {
+  try {
+    await fetch('https://cloud.umami.is/api/send', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        payload: {
+          hostname: request.headers.get('host'),
+          language: request.headers.get('accept-language'),
+          referrer: request.headers.get('referer'),
+          screen: `0x0`,
+          title: 'Server',
+          url: request.url,
+          website: '192417e2-392b-4ac0-bbbe-f1d0c25fb369',
+          name: 'color-change',
+          data: {
+            color,
+          }
+        },
+        type: 'event',
+      }),
+    });
+  } catch {
+    console.error('tracking color change event failed');
+  }
+};
+
 export const handler = {
   async POST(request: Request) {
     const color = (await request.json()).color;
+
     try {
       await fetch(
         `http://${
@@ -14,6 +44,7 @@ export const handler = {
       console.error('hue proxy call failed');
     }
 
+    trackColorChangeEvent(request, color);
     return new Response();
   },
 };
